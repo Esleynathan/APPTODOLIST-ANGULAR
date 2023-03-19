3b3a79f669b694a11b464f2e6538029091cf7e92
Esley <esleynathan@hotmail.com>
2023-03-19 12:36:45 -0300

    Projeto To-do List | Hands-on 09

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -10,21 +10,22 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent {
   
-  public taskList: Array<TaskList> = [
-    {task: "Minha nova Task", checked: true},
-    {task: "Minha nova Task 2", checked: false},
-  ];
+  public taskList: Array<TaskList> = [];
 
   constructor() {}
 
   public deleteItemTaskList(event: number){
-    this.taskList.splice(event, 1);
-    
+    this.taskList.splice(event, 1);    
   }
 
   public deleteAllTaskList(){
     const confirm = window.confirm("Você realmente deseja Deletar tudo?");
     if (confirm)
       this.taskList = [];
+  }
+
+  public setEmitTaskList(event: string){
+    this.taskList.push ({task: event, checked: false})
+
   }
 }
